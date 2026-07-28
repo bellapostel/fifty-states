f342f35a2ad5775d123254ae00eb9457c9fb3549
Bella Postel <bellapostelteam@gmail.com>
2026-07-28 13:06:52 -0400

    SD file update

---
+++ analyses/SD_leg_2020/03_sim_SD_ssd_2020.R
@@ -0,0 +1,89 @@
+###############################################################################
+# Simulate plans for `SD_ssd_2020` SSD
+# © ALARM Project, June 2026
+###############################################################################
+
+# Run the simulation -----
+cli_process_start("Running simulations for {.pkg SD_ssd_2020}")
+
+# TODO any pre-computation (VRA targets, etc.)
+
+# TODO customize as needed. Recommendations:
+#  - For many districts / tighter population tolerances, try setting
+#  `pop_temper=0.01` and nudging upward from there. Monitor the output for
+#  efficiency!
+#  - Monitor the output (i.e. leave `verbose=TRUE`) to ensure things aren't breaking
+#  - Don't change the number of simulations unless you have a good reason
+#  - If the sampler freezes, try turning off the county split constraint to see
+#  if that's the problem.
+#  - Ask for help!
+set.seed(2020)
+
+# TODO set equal to one third of number of districts, increase by 10-15 if no convergence
+mh_accept_per_smc <- ceiling(n_distinct(map_ssd$ssd_2020)/3) + 28
+
+plans <- redist_smc(
+  map_ssd,
+  nsims = 5000, runs = 5,
+  constraints = constr_ssd,
+  counties = pseudo_county,
+  sampling_space = "linking_edge",
+  ms_params = list(frequency = 1L, mh_accept_per_smc = mh_accept_per_smc),
+  split_params = list(splitting_schedule = "any_valid_sizes"),
+  verbose = TRUE
+)
+
+# IF CORES OR OTHER UNITS HAVE BEEN MERGED:
+# make sure to call `pullback()` on this plans object!
+
+plans <- plans |>
+    group_by(chain) |>
+    filter(as.integer(draw) < min(as.integer(draw)) + 2000) |> # thin samples
+    ungroup()
+plans <- match_numbers(plans, "ssd_2020")
+
+cli_process_done()
+cli_process_start("Saving {.cls redist_plans} object")
+
+# TODO add any reference plans that aren't already included
+
+# Output the redist_map object. Do not edit this path.
+write_rds(plans, here("data-out/SD_2020/SD_ssd_2020_plans.rds"), compress = "xz")
+cli_process_done()
+
+# Bella added below (uncomment when viewing validation)
+plans <- readRDS("data-out/SD_2020/SD_ssd_2020_plans.rds")
+
+# Compute summary statistics -----
+cli_process_start("Computing summary statistics for {.pkg SD_ssd_2020}")
+
+plans <- add_summary_stats(plans, map_ssd)
+
+# Output the summary statistics. Do not edit this path.
+save_summary_stats(plans, "data-out/SD_2020/SD_ssd_2020_stats.csv")
+
+cli_process_done()
+
+if (interactive()) {
+    library(ggplot2)
+    library(patchwork)
+
+    validate_analysis(plans, map_ssd)
+    summary(plans)
+
+    # Extra validation plots for custom constraints -----
+    # TODO remove this section if no custom constraints
+
+  visual <- redist.plot.distr_qtys(plans, vap_aian / total_vap,
+                           color_thresh = NULL,
+                           color = ifelse(subset_sampled(plans)$ndv > subset_sampled(plans)$nrv, '#3D77BB', '#B25D4C'),
+                           size = 0.5, alpha = 0.5) +
+      scale_y_continuous('Percent AIAN by VAP') +
+      labs(title = 'Approximate Performance')
+    plans |>
+      subset_sampled() |>
+      group_by(draw) |>
+      summarize(n_aian_perf = sum(vap_aian/total_vap > 0.3 & ndshare > 0.5)) |>
+      count(n_aian_perf)
+
+}
